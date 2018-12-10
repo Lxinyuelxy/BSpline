3acae7be0b273873cf87337b9de6848e992c2b27
Lxinyuelxy <836032851@qq.com>
2018-12-10 21:06:46 +0800

    fix a bug

--- sketch.js
+++ sketch.js
@@ -57,11 +57,12 @@ function generateUnclampedKnots(pointsNum, degree) {
 
 function generateClampedKnots(pointsNum, degree) {
   var knots = [];
+  var delta = 1.0 / (pointsNum+degree);
   for(var i = 0; i < pointsNum+degree+1; i++) {
     if (i < degree+1)
       knots[i] = 0.0;
     else if (i < pointsNum)
-      knots[i] = i * 0.1;
+      knots[i] = knots[i-1] + delta;
     else
       knots[i] = 1;
   }
